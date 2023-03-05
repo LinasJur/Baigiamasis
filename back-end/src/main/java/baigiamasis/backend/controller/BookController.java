0fc89930a8efa1da67980971932c56864f197102
LinasJur <jurgeleviciuslinas@gmail.com>
2023-03-05 17:10:48 +0200

    Pirmas komitas su darbo pradzia

---
+++ back-end/src/main/java/baigiamasis/backend/controller/BookController.java
@@ -0,0 +1,83 @@
+package baigiamasis.backend.controller;
+
+
+import baigiamasis.backend.model.Book;
+import baigiamasis.backend.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class BookController {
+
+
+    @Autowired
+    private BookRepository bookRepository;
+
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        try {
+            List<Book> bookList = new ArrayList<>();
+            bookRepository.findAll().forEach(bookList::add);
+
+            if(bookList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+
+        } catch (Exception exception) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+
+        Book bookToSave = bookRepository.save(book);
+        return new ResponseEntity<>(bookToSave, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/updateBookById/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book newBook){
+        Optional<Book> oldData = bookRepository.findById(id);
+
+        if (oldData.isPresent()){
+
+            Book newBookData = oldData.get();
+
+            newBookData.setTitle(newBook.getTitle());
+            newBookData.setAuthor(newBook.getAuthor());
+            newBookData.setDescription(newBook.getDescription());
+            newBookData.setGenre(newBook.getGenre());
+            newBookData.setLenght(newBook.getLenght());
+            newBookData.setPrice(newBook.getPrice());
+            newBookData.setComment(newBook.getComment());
+
+            Book bookData = bookRepository.save(newBookData);
+            return new ResponseEntity<>(bookData, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @DeleteMapping("/deleteBookById/{id}")
+    public ResponseEntity<HttpStatus> deleteBookById(@PathVariable Long id) {
+
+        bookRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+}
---
+++ back-end/src/main/java/baigiamasis/backend/model/Book.java
@@ -0,0 +1,50 @@
+package baigiamasis.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "BOOK")
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Column(name = "AUTHOR", nullable = false)
+    private String author;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @Column(name = "GENRE", nullable = false)
+    private String genre;
+
+    @Column(name = "LENGHT", nullable = false)
+    private Long lenght;
+
+    @Column(name = "PRICE", nullable = false)
+    private Long price;
+
+    @Column(name = "COMMENT", nullable = false)
+    private String comment;
+
+    @Column(name = "CREATE_DATE")
+    private LocalDateTime createDate = LocalDateTime.now();
+
+
+
+}
